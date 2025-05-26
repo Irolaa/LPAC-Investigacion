5ebbaf4c02a9813e98b8738983e8234efb3a65f8
irolaa <irolaluis20@gmail.com>
2025-05-25 20:34:04 -0600

    Update archivo

--- demo-angular/demostracion-a11y-wcag/src/app/app.component.ts
+++ demo-angular/demostracion-a11y-wcag/src/app/app.component.ts
@@ -1,12 +1,15 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demostracion-a11y-wcag';
+  menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
